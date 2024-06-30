141b78f902427be85126567ef065cf9f331ca3d5
Diah082 <152632332+Diah082@users.noreply.github.com>
2024-06-30 23:16:40 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget -q https://raw.githubusercontent.com/sabdo-dadi/premium/main/Cfg/rclone.conf
 mv rclone.conf /root/.config/rclone/rclone.conf
+clear
 echo -e "RCLONE BERHASIL DIUPDATE" | lolcat
 sleep 5
 exit 0
